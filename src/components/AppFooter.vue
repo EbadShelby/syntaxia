d4afa128c380afc3e83a31f807093b13509be674
Yasser Ebad <ebadyasser123xd@gmail.com>
2026-09-01 07:57:37 +0800

    rename request snippet to request cheatsheet

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -38,7 +38,7 @@
           Feedback
         </a>
         <a
-          href="https://github.com/EbadShelby/syntaxia/issues/new?title=[Snippet%20Request]%20"
+          href="https://github.com/EbadShelby/syntaxia/issues/new?title=[Cheatsheet%20Request]%20"
           target="_blank"
           rel="noopener noreferrer"
           class="flex items-center gap-1.5 text-neutral-400 hover:text-primary-lightgreen transition-colors"
@@ -57,7 +57,7 @@
               d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z"
             />
           </svg>
-          Request Snippet
+          Request Cheatsheet
         </a>
         <a
           href="https://github.com/EbadShelby/syntaxia/issues/new?title=[Bug%20Report]%20"
